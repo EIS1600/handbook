61c16b182e92a317cb5d8255bd4b9b2e76d97470
Maxim Romanov <romanov.maxim@gmail.com>
2022-10-20 23:31:30 +0200

    Update the book

---
+++ temp.R
@@ -0,0 +1,17 @@
+libary(historydata)
+
+
+head(historydata::us_state_populations)
+
+head(historydata::us_national_population)
+
+
+
+library(gapminder)
+
+test <- (gapminder::gapminder)
+
+
+plot(test$lifeExp, test$gdpPercap)
+
+plot()
